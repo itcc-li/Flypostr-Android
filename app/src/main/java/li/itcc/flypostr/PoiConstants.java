472ce36eff3eda724acba6158ee1e3c3daaea369
Arthur26 <arthur.marxer@gmail.com>
2016-08-17 18:07:45 +0200

    Test commit

--- app/src/main/java/li/itcc/flypostr/PoiConstants.java
+++ app/src/main/java/li/itcc/flypostr/PoiConstants.java
@@ -4,7 +4,7 @@ package li.itcc.flypostr;
  * Created by Arthur on 12.09.2015.
  */
 public class PoiConstants {
-    public static final int POI_NAME_LENGTH_MIN = 3;
+    public static final int POI_NAME_LENGTH_MIN = 4;
     public static final int POI_NAME_LENGTH_MAX = 100;
     public static final int POI_COMMENT_LENGTH_MAX = 4000;
 
