ecc9fcf1dcb2f68785c1abe3ca723106ac8eda95
Arthur26 <arthur.marxer@gmail.com>
2016-08-18 13:55:48 +0200

    Add Google Sign in (first version)

---
+++ app/src/main/java/li/itcc/flypostr/auth/ProgressDialogActivity.java
@@ -0,0 +1,36 @@
+package li.itcc.flypostr.auth;
+
+import android.app.ProgressDialog;
+import android.support.annotation.VisibleForTesting;
+import android.support.v7.app.AppCompatActivity;
+
+import li.itcc.flypostr.R;
+
+public class ProgressDialogActivity extends AppCompatActivity {
+
+    @VisibleForTesting
+    public ProgressDialog mProgressDialog;
+
+    public void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage(getString(R.string.loading));
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
+
+    public void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        hideProgressDialog();
+    }
+
+}
