6b4bc7c2ee2ce761a585f3c964c7da66cc3fdecf
Sandro Pedrett <sandro.pedrett@hotmail.com>
2016-08-19 21:44:10 +0200

    Remove unused code

--- app/src/main/java/li/itcc/flypostr/poidetail/PoiDetailActivity.java
+++ app/src/main/java/li/itcc/flypostr/poidetail/PoiDetailActivity.java
@@ -23,16 +23,13 @@ import li.itcc.flypostr.backend.PoiOverviewBean;
 /**
  * Created by Arthur on 12.09.2015.
  */
-public class PoiDetailActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<PoiDetailBean> {
+public class PoiDetailActivity extends AppCompatActivity {
     private static final String KEY_ID = "KEY_ID";
     private TextView fName;
-    private EditText fComment;
     private RatingBar fRating;
-    private Location fLocation;
     private ImageView fImage;
     private ProgressBar fProgressBar;
     private TextView fDescription;
-    private Loader<PoiDetailBean> fLoader;
 
     public static void start(Activity parent, String poiId) {
         Intent i = new Intent(parent, PoiDetailActivity.class);
@@ -44,6 +41,7 @@ public class PoiDetailActivity extends AppCompatActivity implements LoaderManage
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.poi_detail_activity);
+
         fName = (TextView)findViewById(R.id.txv_name);
         fDescription = (TextView)findViewById(R.id.txv_description);
         fRating = (RatingBar)findViewById(R.id.rbr_rating);
@@ -52,40 +50,8 @@ public class PoiDetailActivity extends AppCompatActivity implements LoaderManage
         fProgressBar = (ProgressBar)findViewById(R.id.prb_progress);
         fProgressBar.setIndeterminate(true);
         fProgressBar.setVisibility(View.VISIBLE);
-        String id = getIntent().getExtras().getString(KEY_ID);
-        getSupportLoaderManager().initLoader(0, PoiDetailLoader.createArgs(id), this);
+        String id = getIntent().getExtras().getString(KEY_ID); // todo
     }
 
-    @Override
-    public Loader<PoiDetailBean> onCreateLoader(int id, Bundle args) {
-        return new PoiDetailLoader(this, id, args);
-    }
 
-    @Override
-    public void onLoadFinished(Loader<PoiDetailBean> loader, PoiDetailBean data) {
-        fProgressBar.setVisibility(View.GONE);
-        if (data == null) {
-
-        }
-        else {
-            if (data.getImageUrl() != null) {
-                // the image has been downloaded
-                ImageStore store = new ImageStore(this);
-                ImageStore.Key key = store.createKey(data.getOverview().getUuid(), data.getImageUpdateTime());
-                Bitmap bmp = BitmapFactory.decodeFile(store.getImageFilePath(key));
-                fImage.setImageBitmap(bmp);
-            } else {
-                fImage.setImageDrawable(null);
-            }
-            PoiOverviewBean overviewData = data.getOverview();
-            fRating.setRating(overviewData.getRating());
-            fName.setText(overviewData.getName());
-            fDescription.setText(data.getDescription());
-        }
-    }
-
-    @Override
-    public void onLoaderReset(Loader<PoiDetailBean> loader) {
-
-    }
 }
