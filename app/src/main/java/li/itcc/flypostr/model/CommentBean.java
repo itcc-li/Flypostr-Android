a9d04145c312709af96a07ac6873784134bccf28
Sandro Pedrett <sandro.pedrett@hotmail.com>
2016-08-20 09:31:08 +0200

    Insert bean objects

---
+++ app/src/main/java/li/itcc/flypostr/model/CommentBean.java
@@ -0,0 +1,8 @@
+package li.itcc.flypostr.model;
+
+/**
+ * Created by sandro.pedrett on 20.08.2016.
+ */
+
+public class CommentBean {
+}
---
+++ app/src/main/java/li/itcc/flypostr/model/PostingBean.java
@@ -0,0 +1,23 @@
+package li.itcc.flypostr.model;
+
+/**
+ * Created by sandro.pedrett on 20.08.2016.
+ */
+
+public class PostingBean {
+    private String text;
+    private String authorId;
+    private String
+
+    public PostingBean() {
+
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
